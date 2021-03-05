add43250f202775b57705008bcfa93a3d1dab153
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-05 23:16:17 +0100

    updated client CartSummary

--- client/src/components/CartSummary.js
+++ client/src/components/CartSummary.js
@@ -9,5 +9,20 @@ export default function CartSummary() {
 
     const toggleModal = () => setOpen(!isOpen);
 
-
+    return (
+        <>
+            <nav
+                onClick={toggleModal}
+                className="md:ml-auto flex flex-wrap items-center text-base justify-center"
+            >
+                <span className="mr-5 hover:text-white flex items-center">
+                    <CartIcon />
+                    <span className="ml-3">
+                        {formattedTotalPrice} ({cartCount})
+          </span>
+                </span>
+            </nav>
+            <CartModal isOpen={isOpen} toggleModal={toggleModal} />
+        </>
+    );
 }
