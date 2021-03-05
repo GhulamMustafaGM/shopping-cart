043c88df759f4ad44bb44aa925e122f14ec5b091
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-05 23:13:45 +0100

    updated client AddCart

--- client/src/components/AddToCart.js
+++ client/src/components/AddToCart.js
@@ -10,5 +10,12 @@ export default function AddToCart({ product }) {
         toast.success(`${product.name} added to cart!`);
     }
 
-
+    return (
+        <button
+            className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded"
+            onClick={handleAddItem}
+        >
+            Add To Cart
+        </button>
+    );
 }
